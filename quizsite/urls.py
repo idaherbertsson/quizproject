32b299761243f888a1de775e5af1e961fab1b084
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-27 15:17:40 +0200

    Completed page updated
    Completed page updated and a bug (mis of a /) in url is updated

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -5,7 +5,7 @@ from quiz import views
 urlpatterns = [
 	path("", views.startpage),
 	path("quiz/<int:quiz_number>/", views.quiz),
-	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question), 
-	path("quiz/<int:quiz_number>/completed", views.completed),
+	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
+	path("quiz/<int:quiz_number>/completed/", views.completed),
 	]
 
