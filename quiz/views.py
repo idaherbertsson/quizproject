f0d61dc9f6afc73521730cbb21ff8fbfe9f0551d
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-27 14:44:31 +0200

    Quiz pages data from list
    Now the quiz data gets test data

--- quiz/views.py
+++ quiz/views.py
@@ -4,7 +4,7 @@ quizzes = [
 	{
 	"quiz_number" : 1,
 	"name" : "Klassiska böcker",
-	"description" : "Hurbra kan du dina klassiker?"
+	"description" : "Hur bra kan du dina klassiker?"
 	},
 	{
 	"quiz_number" : 2,
@@ -14,7 +14,7 @@ quizzes = [
 	{
 	"quiz_number" : 3,
 	"name" : "Världens mest kända hackare",
-	"description" : "Kan du din hackeristorià?"
+	"description" : "Kan du din hackeristoria?"
 	}]
 
 def startpage (request):
@@ -24,7 +24,11 @@ def startpage (request):
 	return render(request, "startpage.html", context)
 
 def quiz(request, quiz_number):
-	return render (request, "quiz.html")
+	context = {
+		"quiz" : quizzes[quiz_number -1],
+		"quiz_number" : quiz_number,
+	}
+	return render (request, "quiz.html", context)
 
 def question (request, quiz_number, question_number):
 	return render (request, "question.html")
