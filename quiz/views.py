c9c34bbc3973c9bd5d0c2fd581b3e29d5901f497
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-27 12:19:08 +0200

    Fixed bug
    Fixed missing : in views

--- quiz/views.py
+++ quiz/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def startpage (request)
+def startpage (request):
 	return render(request, "startpage.html")
 
-def quiz(request, quiz_number)
+def quiz(request, quiz_number):
 	return render (request, "quiz.html")
 
-def question (request, quiz_number, question_number)
+def question (request, quiz_number, question_number):
 	return render (request, "question.html")
 
-def completed (request, quiz_number)
+def completed (request, quiz_number):
 	return render (request, "completed.html")
