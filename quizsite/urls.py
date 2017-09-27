b31866180245cf9788bd92863ad1ea920d558e24
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-27 12:16:12 +0200

    Fixed bug url
    Missed a , in one of the urls

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -2,10 +2,10 @@
 from django.urls import path
 from quiz import views
 
-urlpatterns [
+urlpatterns = [
 	path("", views.startpage),
 	path("quiz/<int:quiz_number>/", views.quiz),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question), 
-	path("quiz/<int:quiz_number>/completes" views.completed),
+	path("quiz/<int:quiz_number>/completed", views.completed),
 	]
 
