5de23131c33ea4169cd8299c45b31f9a53155965
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-28 15:45:19 +0200

    Completed page def
    Completed page def updated and created loop to get to completed or next question

--- quiz/views.py
+++ quiz/views.py
@@ -34,10 +34,21 @@ def question(request, quiz_number, question_number):
 	return render (request, "question.html", context)
 
 def completed (request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number = quiz_number)
+	questions = list(quiz.questions.all())
+	saved_answers = request.session.get(str(quiz_number), {})
+	num_correct_answers = 0
+
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number)-1].correct
+
+		if correct_answer == answer:
+			num_correct_answers = num_correct_answers+1
+
+	num_questions = quiz.questions.count()
 	context = {
-		"correct" : 12,
-		"total" : 20,
-		"quiz_number" : quiz_number,
+		"correct" : num_correct_answers,
+		"total" : num_questions,
 	}
 	return render (request, "completed.html", context)
 
@@ -46,12 +57,10 @@ def answer(request, quiz_number, question_number):
 	saved_answers = request.session.get(str(quiz_number), {})
 	saved_answers[question_number] = int(answer)
 	request.session[quiz_number] = saved_answers
-	
 
 	quiz = Quiz.objects.get(quiz_number=quiz_number)
 	num_questions = quiz.questions.count()
 	if num_questions <= question_number:
 		return redirect("completed_page", quiz_number)
 	else:
-		return redirect("question_page", quiz_number, question_number + 1)
-
+		return redirect("question_page", quiz_number, question_number+1)
