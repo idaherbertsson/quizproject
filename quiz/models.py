c77693215adad29fdf9f6e1bf669b0f53265b97c
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-28 11:05:13 +0200

    Updated models.py
    Now admin page shows quiz names instead of only saying quiz object

--- quiz/models.py
+++ quiz/models.py
@@ -5,6 +5,9 @@ class Quiz (models.Model):
 	name = models.CharField(max_length = 100)
 	description = models.TextField()
 
+	def __str__(self):
+		return self.name
+
 class Question(models.Model):
 	question = models.TextField()
 	answer1 = models.CharField(max_length=100)
