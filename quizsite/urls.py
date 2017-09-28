3116370f0284a82241e7f6b87f4d712bed0335fa
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-28 10:45:37 +0200

    Added url to admin
    added url to admin.site on urls.py

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,4 +1,5 @@
 
+from django.contrib import admin
 from django.urls import path
 from quiz import views
 
@@ -7,5 +8,6 @@ urlpatterns = [
 	path("quiz/<int:quiz_number>/", views.quiz, name = "quiz_page"),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name = "question_page"),
 	path("quiz/<int:quiz_number>/completed/", views.completed, name = "completed_page"),
+	path("admin/", admin.site.urls),
 	]
 
