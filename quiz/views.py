9db60945ecb855dbb5fbde379c23af852d0c719a
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-28 14:42:03 +0200

    Answer view added

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,5 @@
-
+from django.shortcuts import redirect
 from quiz.models import Quiz
-
 from django.shortcuts import render
 
 
@@ -32,8 +31,6 @@ def question(request, quiz_number, question_number):
 		"quiz_number" : quiz_number,
 	}
 
-
-
 	return render (request, "question.html", context)
 
 def completed (request, quiz_number):
@@ -43,3 +40,10 @@ def completed (request, quiz_number):
 		"quiz_number" : quiz_number,
 	}
 	return render (request, "completed.html", context)
+
+def answer(request, quiz_number, question_number):
+	answer = request.POST["answer"]
+	saved_answers = request.session.get(str(quiz_number),{})
+	saved_answers[question_number] = int(answer)
+	request.session[quiz_number] = saved_answers
+	return redirect("question_page", quiz_number, question_number + 1)
