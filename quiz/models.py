d513c05ab91e9fb80bc8288706dcca5fdb91d482
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-28 10:39:01 +0200

    Fixed missing parenthesis in models
    Added missing parenthesis and : in models.py, then migrated to db

--- quiz/models.py
+++ quiz/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Quiz (models.Model)
-	quiz_number = models.PositiveIntegerField
+class Quiz (models.Model):
+	quiz_number = models.PositiveIntegerField()
 	name = models.CharField(max_length = 100)
-	description = models.TextField
+	description = models.TextField()
 
-class Question(models.Model)
+class Question(models.Model):
 	question = models.TextField()
 	answer1 = models.CharField(max_length=100)
 	answer2 = models.CharField(max_length=100)
