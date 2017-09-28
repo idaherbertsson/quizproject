094febacf8f5c1b0066dd9e76e1f707187c9d0f9
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-28 12:00:59 +0200

    Startpage data from db
    Getting startpage data from database. deleted and then again added render in the def of startpage, since i got an error

--- quiz/views.py
+++ quiz/views.py
@@ -1,27 +1,15 @@
+
+from quiz.models import Quiz
+
 from django.shortcuts import render
 
-quizzes = [
-	{
-	"quiz_number" : 1,
-	"name" : "Klassiska böcker",
-	"description" : "Hur bra kan du dina klassiker?"
-	},
-	{
-	"quiz_number" : 2,
-	"name" : "Största fotbollslagen",
-	"description" : "Kan du dina lag?"
-	},
-	{
-	"quiz_number" : 3,
-	"name" : "Världens mest kända hackare",
-	"description" : "Kan du din hackeristoria?"
-	}]
 
 def startpage (request):
 	context = {
-		"quizzes" : quizzes
+		"quizzes": Quiz.objects.all(),
+
 	}
-	return render(request, "startpage.html", context)
+	return render (request, "startpage.html", context)
 
 def quiz(request, quiz_number):
 	context = {
