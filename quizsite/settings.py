99a21ee62a0f4c2e81e70f619121648a222dfc91
idaherbertsson <32261365+idaherbertsson@users.noreply.github.com>
2017-09-29 10:15:41 +0200

    Databases updated
    Updated database to PostgreSQL in settings.py

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -73,13 +74,15 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(
+            os.path.join(BASE_DIR, 'db.sqlite3')
+        )
+    )
 }
+
 
 
 # Password validation
